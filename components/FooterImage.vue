df5ef81f6420944993e6c982de27e0581eaef328
Julia Sharp <julia.sharp@salesforce.com>
2024-08-19 12:55:15 -0700

    add footer cta

--- components/FooterImage.vue
+++ components/FooterImage.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import LogoSVG from '/src/alt-logo-white.svg?component';
 const currentYear = computed(() => new Date().getFullYear());
 </script>
 
 <template>
   <div class="footer-bottom">
+    <div class="footer-logo">
+      <!-- <LogoSVG /> -->
+    </div>
+    <p class="footer-cta">THE FUTURE CAN BE JUST - DESIGN IT !</p>
     <div class="footer-img">
-      <!-- <NuxtImg src="/just-team.jpg" /> -->
     </div>
     <div class="footer-bottom-content">
-      <!-- <p>THE FUTURE CAN BE JUST - DESIGN IT !</p> -->
       <hr class="footer-divider">
       <p class="copyright">copyright &copy; just {{ currentYear }}</p>
     </div>
@@ -40,9 +43,30 @@ const currentYear = computed(() => new Date().getFullYear());
   }
 }
 
+.footer-cta {
+  color: #C6F250;
+  font-family: 'Inter Bold';
+  position: absolute;
+  font-size: 28px;
+  bottom: 58px;
+  width: 100%;
+  text-align: center;
+  padding: 0 50px;
+  @media (max-width: 1000px) {
+    font-size: 25px;
+  }
+  @media (max-width: 767px) {
+    font-size: 18px;
+    padding: 0 20px;
+  }
+}
+
 .footer-divider {
   margin: 0 50px;
   border: 0.5px solid #C6F250;
+  @media (max-width: 767px) {
+    margin: 0 20px;
+  }
 }
 
 .copyright {
@@ -52,5 +76,9 @@ const currentYear = computed(() => new Date().getFullYear());
   font-size: 14px;
   padding: 14px 0;
   color: #C6F250;
+  @media (max-width: 767px) {
+    margin: 0 20px;
+    font-size: 12px;
+  }
 }
 </style>
