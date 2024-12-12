fbdb9154771f3b9dbfe292e17362149d710bbc99
Julia Sharp <julia.sharp@salesforce.com>
2024-12-12 15:23:21 -0800

    project formatting updates

--- components/Projects.vue
+++ components/Projects.vue
@@ -67,12 +67,12 @@ const selectProject = (name) => {
 };
 
 const settings = {
-  itemsToShow: 3,
+  itemsToShow: 2,
   snapAlign: 'start',
   breakpoints: {
-    900: {
-      itemsToShow: 2,
-    },
+    // 900: {
+    //   itemsToShow: 3,
+    // },
   }
 };
 
@@ -111,8 +111,11 @@ onBeforeUnmount(() => {
             <template v-if="isSmallViewport">
               <Carousel v-bind="settings">
                 <Slide v-for="(project, index) in projects" :key="project.name">
-                  <a @click="selectProject(project.name)">{{ project.name }}</a>
+                  <a @click="selectProject(project.name)" :class="{ active: activeProject === project.name }">{{ project.name }}</a>
                 </Slide>
+                <template #addons>
+                  <Navigation class="project-arrows"/>
+                </template>
               </Carousel>
             </template>
             <template v-else>
@@ -124,9 +127,10 @@ onBeforeUnmount(() => {
         </div>
         <div class="project-right">
           <div class="project-info" v-for="(project, index) in projects" :key="carouselKey" v-show="selectedProject === project.name">
-            <Carousel :items-to-show="1" :wrap-around="true" :autoplay="2500" :key="project.name">
+            <!-- <Carousel :items-to-show="1" :wrap-around="true" :autoplay="2500" :key="project.name"> -->
+              <Carousel :items-to-show="1" :wrap-around="true" :key="project.name">
               <Slide v-for="(image, imgIndex) in project.images" :key="imgIndex">
-                <img :src="image.link" :alt="image.altText" />
+                <img class="project-img" :src="image.link" :alt="image.altText" />
               </Slide>
             </Carousel>
             <span class="project-date">{{ project.date }}</span>
@@ -165,6 +169,9 @@ onBeforeUnmount(() => {
   }
   &-container {
     padding: 80px 60px;
+    @media (max-width: 1490px) {
+      padding:  60px 60px;
+    }
     @media (max-width: 1180px) {
       padding:  40px 50px;
     }
@@ -197,7 +204,7 @@ onBeforeUnmount(() => {
     flex-shrink: 0;
     position: relative;
     @media (max-width: 1600px) {
-      flex-basis: 260px;
+      flex-basis: 275px;
     }
     @media (max-width: 1280px) {
       flex-basis: 230px;
@@ -209,10 +216,22 @@ onBeforeUnmount(() => {
       content: '';
       position: absolute;
       background-color: #C6F250;
-      @media (min-width: 1181px) {
+      @media (min-width: 1281px) and (max-width: 1600px) {
+        height: calc(100% + 80px);
+        top: -40px;
+        right: 35px;
+        width: 1px;
+      }
+      @media (min-width: 1601px) {
         height: calc(100% + 120px);
         top: -60px;
         right: 35px;
+        width: 1px;
+      }
+      @media (max-width: 1280px) {
+        height: calc(100% + 80px);
+        right: 10px;
+        top: -40px;
         width: 1px;
       }
       @media (max-width: 1180px) {
@@ -224,6 +243,7 @@ onBeforeUnmount(() => {
       @media (max-width: 760px) {
         width: calc(100% + 20px);
         left: -10px;
+        top: 105px;
       }
     }
     h3 {
@@ -235,10 +255,8 @@ onBeforeUnmount(() => {
       @media (max-width: 1600px) {
         font-size: 110px;
       }
-      @media (max-width: 1280px) {
-        font-size: 100px;
-      }
       @media (max-width: 1180px) {
+        font-size: 100px;
         text-align: center;
       }
       @media (max-width: 767px) {
@@ -252,19 +270,23 @@ onBeforeUnmount(() => {
     margin-top: 25px;
     position: relative;
     @media (min-width: 1181px) {
-      flex-basis: 320px;
+      flex-basis: 365px;
       padding-right: 75px;
-      padding-left: 12px;
+      padding-left: 50px;
       flex-shrink: 0;
     }
     @media (min-width: 1601px) {
-      flex-basis: 380px;
+      flex-basis: 465px;
+      padding-left: 45px;
+      padding-right: 75px;
     }
     @media (max-width: 1180px) {
       margin-top: 50px;
+      margin-bottom: 70px;
     }
     @media (max-width: 760px) {
-      margin-bottom: 24px;
+      margin-top: 40px;
+      margin-bottom: 40px;
     }
     &:before {
       content: '';
@@ -272,7 +294,13 @@ onBeforeUnmount(() => {
       background-color: #C6F250;
       top: -85px;
       right: 80px;
-      @media (min-width: 1181px) {
+      @media (min-width: 1181px) and (max-width: 1600px) {
+        top: -65px;
+        right: 80px;
+        width: 1px;
+        height: calc(100% + 104px);
+      }
+      @media (min-width: 1601px) {
         top: -85px;
         right: 80px;
         width: 1px;
@@ -297,12 +325,16 @@ onBeforeUnmount(() => {
       color: #C6F250;
       font-size: 32px;
       padding-bottom: 90px;
+      position: relative;
       @media (max-width: 1600px) {
-        font-size: 2vw;
-        padding-bottom: 6vw;
+        font-size: 28px;
+        padding-bottom: 5.5vw;
       }
       @media (max-width: 1280px) {
         font-size: 25px;
+      }
+      @media (max-width: 1180px) {
+        padding-bottom: 0;
       }
       @media (max-width: 900px) {
         font-size: 20px;
@@ -314,21 +346,44 @@ onBeforeUnmount(() => {
         font-size: 17px;
       }
       &.active {
-        // &:before {
-        //   content: '';
-        //   background: url('/src/asterisk.svg');
-        //   width: 23px;
-        //   height: 23px;
-        //   position: absolute;
-        //   top: 3px;
-        //   left: -21px;
-        // }
+        &:before {
+          content: '';
+          background: url('/src/asterisk.svg');
+          width: 19px;
+          height: 17.5px;
+          position: absolute;
+          top: 7px;
+          left: -33px;
+        }
       }
     }
   }
 }
 
+.projects-names a {
+  position: relative;
+  @media (max-width: 760px) {
+    max-width: 123px;
+  }
+  &.active:before {
+    content: '';
+    background: url('/src/asterisk.svg');
+    width: 19px;
+    height: 17.5px;
+    position: absolute;
+    top: 7px;
+    left: -33px;
+    @media (max-width: 760px) {
+      top: 4px;
+      left: -25px;
+    }
+  }
+}
+
 .project {
+  &-img {
+    height: 100%;
+  }
   &-date {
     font-family: 'Calling Code';
     font-size: 18px;
@@ -347,21 +402,33 @@ onBeforeUnmount(() => {
       text-transform: uppercase;
       display: block;
       margin: 40px 0 25px;
+      @media (max-width: 1600px) {
+        margin: 25px 0 20px;
+      }
     }
     h5 {
       font-family: 'Inter Bold';
       font-size: 40px;
       margin-bottom: 34px;
+      @media (max-width: 1600px) {
+        font-size: 32px;
+        margin-bottom: 20px;
+      }
       @media (max-width: 1280px) {
         font-size: 28px;
-        margin-bottom: 20px;
       }
     }
     p {
       font-size: 20px;
       line-height: 1.4;
+      @media (max-width: 1600px) {
+        font-size: 18px;
+      }
       @media (max-width: 1280px) {
         font-size: 17px;
+      }
+      @media (max-width: 760px) {
+        font-size: 16px;
       }
     }
   }
@@ -380,10 +447,10 @@ onBeforeUnmount(() => {
   &-2 {
     @media (min-width: 1181px) {
       top: 17px;
-      left: 251px;
+      left: 276px;
     }
     @media (min-width: 1281px) {
-      left: 281px;
+      left: 295px;
     }
     @media (min-width: 1601px) {
       left: 321px;
@@ -392,6 +459,9 @@ onBeforeUnmount(() => {
       top: 157px;
       left: 17px;
     }
+    @media (max-width: 760px) {
+      top: 142px;
+    }
   }
   &-3 {
     bottom: 17px;
@@ -399,11 +469,11 @@ onBeforeUnmount(() => {
   }
   &-4 {
     @media (min-width: 1181px) {
-      left: 251px;
+      left: 276px;
       bottom: 17px;
     }
     @media (min-width: 1281px) {
-      left: 281px;
+      left: 295px;
     }
     @media (min-width: 1601px) {
       left: 321px;
@@ -411,6 +481,9 @@ onBeforeUnmount(() => {
     @media (max-width: 1180px) {
       right: 17px;
       top: 251px;
+    }
+    @media (max-width: 760px) {
+      top: 227px;
     }
   }
   &-5 {
@@ -424,34 +497,61 @@ onBeforeUnmount(() => {
   &-7 {
     @media (min-width: 1181px) {
       top: 17px;
-      left: 526px;
+      left: 571px;
     }
     @media (min-width: 1281px) {
-      left: 557px;
+      left: 616px;
     }
     @media (min-width: 1601px) {
-      left: 656px;
+      left: 741px;
     }
     @media (max-width: 1180px) {
       right: 17px;
       top: 157px;
     }
+    @media (max-width: 760px) {
+      top: 142px;
+    }
   }
   &-8 {
     @media (min-width: 1181px) {
       bottom: 17px;
-      left: 526px;
+      left: 571px;
     }
     @media (min-width: 1281px) {
-      left: 556px;
+      left: 616px;
     }
     @media (min-width: 1601px) {
-      left: 656px;
+      left: 741px;
     }
     @media (max-width: 1180px) {
       left: 17px;
       top: 251px;
     }
+    @media (max-width: 760px) {
+      top: 227px;
+    }
+  }
+}
+
+.projects-names .carousel__track {
+  @media (max-width: 1180px) {
+    height: 40px;
+  }
+  @media (max-width: 760px) {
+    height: 50px;
+  }
+}
+
+.project-arrows {
+  &.carousel__prev {
+    left: -15px;
+  }
+  &.carousel__next {
+    right: -15px;
+  }
+  svg {
+    fill: #C6F250;
   }
 }
 </style>
